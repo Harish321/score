0787e11e0a402b97ced5584b9fd8b789c7fb109b
harish.d@dvara.com <harish.d14@iiits.in>
2019-10-22 15:30:29 +0530

    update to 1.0.1

--- score.js
+++ score.js
@@ -10,6 +10,7 @@ const scheduleUrl = "https://www.cricbuzz.com/cricket-series/2697/icc-cricket-wo
 program.option('-m, --match-id <matchId>', 'Match ID')
 .action(()=>{
     if(program.matchId){
+        startNotfication(program.matchId);
     }   
     else{
         var finalArray = [];
@@ -17,9 +18,7 @@ program.option('-m, --match-id <matchId>', 'Match ID')
             if(err)
                 console.log(err);
             else if(body){
-                
                 var html = (HtmlParser.parse(body));
-                // console.log(html.querySelectorAll(".cb-col-60.cb-col.cb-srs-mtchs-tm"))
                 var matches = html.querySelectorAll(".cb-col-60.cb-col.cb-srs-mtchs-tm")
                 for (var i=matches.length-1;i>=0;i--){
                     var p = matches[i].querySelector('.text-hvr-underline')
@@ -55,40 +54,32 @@ var wrapperDisplayNotification = ['display notification "','" with ']
 var wrapperTitle = ['title "','" ']
 var wrapperSubTitle = ['subtitle "','"'] 
 
-
 var bowlerId = 0;
 var showToss = false;
 var showOut = false;
-var bt1 = 0;
-var bt2 = 0;
+var bt1 = {};
+var bt2 = {};
 var prevCom = '';
+var gp;
 
 var getIndiaMatch = function(url){
     request(url,(err,req,body)=>{
-        try{
-            body = JSON.parse(body);
-        }catch (e){
-            return
-        }
-        if(err){
-
-        }
+        try{body = JSON.parse(body)}
+        catch (e){return}
+        if(err) return;
         var comm_lines = body.comm_lines;
         if(body.state == 'mom' || body.state == 'complete'){
             sendNotification(body.status,'',body.state_title,'');
             clearInterval(gp);
-        }
-        else if (body.toss && !showToss){
+        } else if (body.toss && showToss && !body.score){
             showToss = true;
             sendNotification('','',body.toss.winner + ' Won the Toss','Choose to '+body.toss.decision);
-        }
-        else if((comm_lines[0].evt == 'four' || comm_lines[0].evt == 'six') && comm_lines[0].o_no != prevCom){
+        } else if((comm_lines[0].evt == 'four' || comm_lines[0].evt == 'six') && comm_lines[0].o_no != prevCom){
                 prevCom = comm_lines[0].o_no;
                 var score = body.team1.id == body.score.batting.id ? body.team1.s_name : body.team2.s_name;
                 score +=  ' '+ body.score.batting.score;
                 sendNotification('',comm_lines[0].comm.replace('<b>','').replace('</b>','').replace('four','FOUR').replace('six','SIX'),comm_lines[0].evt.toUpperCase(),score);
-        }
-        else if(body.score){
+        } else if(body.score){
             if(body.score.batsman.length > 1){
                 showOut = false;
                 var player1 = {};
@@ -98,36 +89,36 @@ var getIndiaMatch = function(url){
                 player1.score = body.score.batsman[0].r;
                 player1.name += body.score.batsman[0].strike == 1 ? ' *' : '';
                 player1.fn = player1.name + ' '+player1.score+' ('+body.score.batsman[0].b+')';
+                bt1 = player1;
                 var player2 = {};
                 player2.id = body.score.batsman[1].id;
-                bt2 = player2.id;
                 player2.name = (body.players.filter(function(o){return o.id == player2.id}))[0].f_name;
                 player2.score = body.score.batsman[1].r;
                 player2.name += body.score.batsman[1].strike == 1 ? ' *' : '';
                 player2.fn = player2.name + ' '+player2.score+' ('+body.score.batsman[1].b+')';
+                bt2 = player2;
                 var score = body.team1.id == body.score.batting.id ? body.team1.s_name : body.team2.s_name;
                 score +=  ' '+ body.score.batting.score;
-                if (body.score.bowler[0].id != bowlerId){
-                    bowlerId = body.score.bowler[0].id;
+                // if (body.score.bowler[0].id != bowlerId){
+                    // bowlerId = body.score.bowler[0].id;
                     sendNotification(player1.fn+'  |  ',player2.fn,score,body.status)
-                }
+                // }
             }
             else if(showOut== false){
                 showOut = true
                 if(body.score.batsman[0].id == bt1){
-                    sendNotification('','','Wicket',(body.players.filter(function(o){return o.id == bt2}))[0].f_name+' is Out');
+                    sendNotification(comm_lines[0].comm.replace('<b>','').replace('</b>','').replace("'","\\'"),'','Wicket',bt2.fn?bt2.fn : '');
                 }
                 else{
-                    sendNotification('','','Wicket',(body.players.filter(function(o){return o.id == bt1}))[0].f_name+' is Out');
+                    sendNotification(comm_lines[0].comm.replace('<b>','').replace('</b>','').replace("'","\\'"),'','Wicket',bt1.fn ? bt1.fn : "");
                 }
-                bt1 =0;
-                bt2 =0;
+                bt1 ={};
+                bt2 ={};
             }
-        }else if(typeof(body.toss) == 'undefined' && typeof(body.score) == 'undefined' && init){
+        } else if(typeof(body.toss) == 'undefined' && typeof(body.score) == 'undefined' && init){
             init = false;
             sendNotification('','Once Match Started Notification will be sent','Match Not started','');
         }
-        
     })
 }
 
@@ -137,15 +128,11 @@ var sendNotification = function(player1,player2,score,status){
         exesync(wrapperPreText[0]+wrapperDisplayNotification[0]+status+wrapperDisplayNotification[1]+wrapperTitle[0]+score+wrapperTitle[1]+wrapperSubTitle[0]+player1+player2+wrapperSubTitle[1]+wrapperPreText[1]);
     }, 3000);
 }
-// getIndiaMatch()
-// var p = setInterval(function(){
-//     getIndiaMatch()
-// },10000)
-var gp;
+
 var startNotfication = function(id){
     const url = 'https://www.cricbuzz.com/match-api/'+id+'/commentary.json';
     getIndiaMatch(url)
      gp = setInterval(function(){
         getIndiaMatch(url)
-    },10000)
+    },40000)
 }
